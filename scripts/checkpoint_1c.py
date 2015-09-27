139475ae742019170c2a7145ca019accc62b69eb
fmuheim <f.muheim@ed.ac.uk>
2015-09-27 20:36:12 +0100

    1b and 1c

--- scripts/checkpoint_1c.py
+++ scripts/checkpoint_1c.py
@@ -1,11 +1,12 @@
 # Imports
 import webiopi
-     
+
+# Set which GPIO pin is connected to the switch
+#    Insert your code here
+   
 # Read the switch and if it is pressed toggle the state of the LED
 if (GPIO.digitalRead(SWITCH0) == GPIO.LOW):
 
 # Read the value of the LED, invert it and save it to variable value
-#    Insert your code here
-
 # Write variable value to the LED
 #    Insert your code here
