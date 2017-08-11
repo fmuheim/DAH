bc05f480e02ebd5f85aa830f9d3b0f55759cbae3
DoctorFeed <ben.wynne@gmail.com>
2017-08-11 15:28:59 +0100

    Woops reverted

--- scripts/checkpoint_2b.py
+++ scripts/checkpoint_2b.py
@@ -6,7 +6,7 @@ DAC1 = MCP492X(chip=1, channelCount=2, vref=3.3)
 
 # Output 1.3V on channel 0 of DAC1
 print ('output 1.3V on channel 0 of DAC1')
-DAC1.analogWriteVolt(0, 1.7)
+DAC1.analogWriteVolt(0, 1.3)
 
 # Print value of channel 0 of DAC1
 print ('value of register for channel 0 of DAC1')
