d1bd4ca3bad56c15ddde16ea6ba0554fb05cb6cb
DruidFish <ben.wynne@gmail.com>
2017-08-11 15:52:40 +0100

    Missing file

---
+++ scripts/checkpoint_5d.py
@@ -0,0 +1,6 @@
+# Pylab makes graph plotting very easy:
+import pylab
+
+# Make a histogram with NumberOfBins bins 
+# in the range binMinimum to binMaximum
+pylab.hist( someData, bins=NumberOfBins, range=[binMinimum, binMaximum] )
