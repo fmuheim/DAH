606ebbcc37e0be49c23b31cfd7fd357c0b199b46
DruidFish <ben.wynne@gmail.com>
2017-08-11 15:01:07 +0100

    Updated webiopi imports

--- scripts/checkpoint_2b.py
+++ scripts/checkpoint_2b.py
@@ -1,12 +1,12 @@
-# Import ADC and DAC chip libraries
-from webiopi.devices.analog import MCP3208, MCP4922
- 
+# Import DAC chip libraries
+from webiopi.devices.analog.mcp492X import MCP492X
+
 # Define DAC on Chip Enable 1 (CE1/GPIO7)
-DAC1 = MCP4922(chip=1)
+DAC1 = MCP492X(chip=1, channelCount=2, vref=3.3)
 
 # Output 1.3V on channel 0 of DAC1
 print ('output 1.3V on channel 0 of DAC1')
-DAC1.analogWriteVolt(0, 1.3)
+DAC1.analogWriteVolt(0, 1.7)
 
 # Print value of channel 0 of DAC1
 print ('value of register for channel 0 of DAC1')
