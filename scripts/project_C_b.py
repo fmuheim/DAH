42ce679e5655a425524e1438fb29c80cb0cbf2cd
Benjamin Wynne <you@example.com>
2018-07-02 17:35:53 +0100

    Converted mp3 to wav and mpg321 to aplay

--- scripts/project_C_b.py
+++ scripts/project_C_b.py
@@ -1,5 +1,5 @@
 import os
 while True:
     if ( switch1 == True  & switch2 == True ):
-        os.system('mpg321 -q C5.mp3 &')
-        os.system('mpg321 -q C5#.mp3 &')
+        os.system('aplay -q C5.wav &')
+        os.system('aplay -q C5#.wav &')
