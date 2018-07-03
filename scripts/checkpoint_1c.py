cd1f07093ffe36b1172ac5f5965b9d4a20682651
Benjamin Wynne <you@example.com>
2018-07-03 13:32:56 +0100

    Change from webIOPi.GPIO to RPi.GPIO, removing need for sudo

--- scripts/checkpoint_1c.py
+++ scripts/checkpoint_1c.py
@@ -1,11 +1,11 @@
 # Imports
-import webiopi
+import RPi.GPIO as GPIO
 
 # Set which GPIO pin is connected to the switch
 #    Insert your code here
    
 # Read the switch and if it is pressed toggle the state of the LED
-if (GPIO.digitalRead(SWITCH0) == GPIO.LOW):
+if (GPIO.input(SWITCH0) == GPIO.LOW):
 
 # Read the value of the LED, invert it and save it to variable value
 # Write variable value to the LED
