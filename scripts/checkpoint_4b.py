f6b157f9016c41b27f390dca03eab737cc4bdcaf
bwynne <bwynne@cern.ch>
2019-10-15 17:20:26 +0100

    Change the switch in 4b to react to high input, ignoring portWrite interference

--- scripts/checkpoint_4b.py
+++ scripts/checkpoint_4b.py
@@ -1,8 +1,8 @@
 # Loop for ever
 while True:
     # Read the switch and if it is pressed toggle the state of the LED
-    if (mcp.digitalRead(SWITCH0) == GPIO.LOW):
+    if (mcp.digitalRead(SWITCH0) == GPIO.HIGH):
         # Insert your code here
 
         # dummy write to reset switch register
-        mcp.digitalWrite(SWITCH0, GPIO.HIGH)
+        mcp.digitalWrite(SWITCH0, GPIO.LOW)
